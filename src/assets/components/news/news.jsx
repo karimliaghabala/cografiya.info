9192236d12be783f996a991a4b2d26485d0f097f
Karimli Aghabala <54455724+karimliaghabala@users.noreply.github.com>
2023-02-16 18:25:26 +0400

    update scroll

--- src/assets/components/news/news.jsx
+++ src/assets/components/news/news.jsx
@@ -36,6 +36,26 @@ const News = () => {
         <Link to='/'>Davamını oxu</Link>
         </div>
     </div>
+    <div className='news-i'>
+        <div className="img-new">
+          <img src={NewsImg} alt="" />
+        </div>
+        <div className="news-text">
+        <h3 className='h3'>Coğrafiya təbiət  elmləri sistemində</h3>
+        <p>Coğrafiya (yun. γεωγραφία, yerin təsviri, γεια — Yer, torpaq və γραφειν — yazmaq, təsvir etmək) — təbii və istehsal ərazi komplekslərini və onların komponentlərini tədqiq edən təbiət və ictimai elmlər sistemi. </p>
+        <Link to='/'>Davamını oxu</Link>
+        </div>
+    </div>
+    <div className='news-i'>
+        <div className="img-new">
+          <img src={NewsImg} alt="" />
+        </div>
+        <div className="news-text">
+        <h3 className='h3'>Coğrafiya təbiət  elmləri sistemində</h3>
+        <p>Coğrafiya (yun. γεωγραφία, yerin təsviri, γεια — Yer, torpaq və γραφειν — yazmaq, təsvir etmək) — təbii və istehsal ərazi komplekslərini və onların komponentlərini tədqiq edən təbiət və ictimai elmlər sistemi. </p>
+        <Link to='/'>Davamını oxu</Link>
+        </div>
+    </div>
     </>
   )
 }
