0f86509144c95bd16b2e93bc24e735bfcafc70e9
Karimli Aghabala <54455724+karimliaghabala@users.noreply.github.com>
2023-02-27 00:09:45 +0400

    update

--- src/assets/components/categories/tests/ksqbsq.jsx
+++ src/assets/components/categories/tests/ksqbsq.jsx
@@ -6,26 +6,9 @@ const Tests = () => {
     return (
         <div className='tests'>
             <div className="left-t subjects">
-                <Link to='/'>Ibtidai siniflər</Link>
-                <Link to='/'>Azərbaycan tarixi</Link>
-                <Link to='/'>Ümumi tarix</Link>
-                <Link to='/'>Riyaziyyat</Link>
-                <Link to='/'>Azərbaycan dili</Link>
-                <Link to='/'>Texnologiya</Link>
-                <Link to='/'>Həyat bilgisi</Link>
-                <Link to='/'>Musiqi</Link>
-                <Link to='/'>Rus dili</Link>
-
+                <Link to='/geographytest'>Coğrafiya</Link>
             </div>
             <div className="right-t subjects">
-                <Link to='/'>Ədəbiyyat</Link>
-                <Link to='/geographytest'>Coğrafiya</Link>
-                <Link to='/'>Fizika</Link>
-                <Link to='/'>Biologiya</Link>
-                <Link to='/'>Kimya</Link>
-                <Link to='/'>Təsviri İncəsənət</Link>
-                <Link to='/'>Fiziki tərbiyə</Link>
-                <Link to='/'>İngilis dili</Link>
             </div>
         </div>
     )
